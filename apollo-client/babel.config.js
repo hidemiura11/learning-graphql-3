0bf8785b9ddf20c8726bf3e62d0b3779bc6f0695
hidems <miura.eleven@gmail.com>
2024-03-30 01:17:03 +0100

    On Going, issue with vue apollo

--- apollo-client/babel.config.js
+++ apollo-client/babel.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    ['@babel/preset-env', {
+      targets: {
+        browsers: [
+          '> 1%',
+          'last 2 versions',
+          'not dead'
+        ]
+      }
+    }]
+  ],
+  plugins: [
+    '@babel/plugin-transform-runtime'
   ]
-}
+};
--- apollo-client/src/main.js
+++ apollo-client/src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { createProvider } from './vue-apollo'
+// import { createProvider } from './vue-apollo'
 import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
-  apolloProvider: createProvider(),
+  // apolloProvider: createProvider(),
   vuetify,
   render: h => h(App)
 }).$mount('#app')
